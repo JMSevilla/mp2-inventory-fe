f2dc754873f4f4205f1cad7c7e6c5ba1d09ef861
CC <cc@CCs-MacBook-Air.local>
2023-04-07 22:52:43 +0800

    feature/mp2-3-reusable-footer-resolve-conflict

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,5 @@
 export * from './AppBar/Appbar'
 export * from './TextField/TextField'
-<<<<<<< HEAD
 export * from './Footer/Footer'
-=======
 export * from './Button/Button' 
 export * from './Card/Card'
->>>>>>> origin/release/sprint-1

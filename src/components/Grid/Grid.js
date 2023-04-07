3d835e01123c02352e20a51002079ff678facf32
ThisIsAthan <jonathanlatozajr@gmail.com>
2023-04-07 09:59:03 +0800

    My initial commit

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Grid } from '@mui/material'
+
+export const ControlledGrid = (props) => {
+    const { 
+        rowSpacing,
+        columnSpacing,
+        xs,
+        arrMapping = [],
+        style
+    } = props
+    return (
+        <Grid style={style} container rowSpacing={rowSpacing} columnSpacing={rowSpacing}>
+            {
+                arrMapping.map((item) => (
+                    <Grid item xs={xs}>
+                        {item.children}
+                    </Grid>
+                ))
+            }
+        </Grid>
+    )
+    
+}
